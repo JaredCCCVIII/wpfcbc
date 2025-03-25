0d86c969a72b6897102fe504b404f5ad5dcddd07
JaredCCCVIII <esqueleto300806@gmail.com>
2025-03-24 23:55:06 -0500

    updated stats

--- src/main/java/com/ato/shupapi/entities/DaarickCitizen.java
+++ src/main/java/com/ato/shupapi/entities/DaarickCitizen.java
@@ -3,31 +3,23 @@ package com.ato.shupapi.entities;
 import com.ato.shupapi.ShupapiumSounds;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtOps;
-import net.minecraft.nbt.Tag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.Difficulty;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Creeper;
-import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.monster.Zombie;
-import net.minecraft.world.entity.monster.ZombieVillager;
-import net.minecraft.world.entity.npc.Villager;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.monster.*;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.event.ForgeEventFactory;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -39,6 +31,9 @@ public class DaarickCitizen extends Zombie {
     @Override
     protected void registerGoals() {
         super.registerGoals();
+
+        // Make Illagers (Pillagers, Evokers, etc.) attack the Daarick Citizen
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractIllager.class, true));
     }
 
     @Override
@@ -47,7 +42,7 @@ public class DaarickCitizen extends Zombie {
     }
 
     public static AttributeSupplier.@NotNull Builder createAttributes() {
-        return Monster.createMobAttributes().add(Attributes.FOLLOW_RANGE, 64.0F).add(Attributes.MOVEMENT_SPEED, 0.4F).add(Attributes.ATTACK_DAMAGE, 1.0F).add(Attributes.ARMOR, 1.0F).add(Attributes.SPAWN_REINFORCEMENTS_CHANCE);
+        return Monster.createMobAttributes().add(Attributes.MAX_HEALTH, 10.0D).add(Attributes.FOLLOW_RANGE, 64.0F).add(Attributes.MOVEMENT_SPEED, 0.4F).add(Attributes.ATTACK_DAMAGE, 1.0F).add(Attributes.ARMOR, 1.0F).add(Attributes.SPAWN_REINFORCEMENTS_CHANCE);
     }
 
     @Override
@@ -162,7 +157,7 @@ public class DaarickCitizen extends Zombie {
 
     @Override
     public @NotNull MobType getMobType() {
-        return MobType.ILLAGER;
+        return MobType.UNDEFINED;
     }
 
     @Override
