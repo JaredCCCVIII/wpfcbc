8416d14518d42984c0296257121c6d50e04b8162
JaredCCCVIII <esqueleto300806@gmail.com>
2025-05-31 09:56:14 -0500

    coming update

--- src/main/java/com/ato/shupapi/items/BattleArtillerySolidGunAmmoItem.java
+++ src/main/java/com/ato/shupapi/items/BattleArtillerySolidGunAmmoItem.java
@@ -45,7 +45,7 @@ public class BattleArtillerySolidGunAmmoItem extends Item implements AutocannonA
 
     @Override
     public ItemStack getSpentItem(ItemStack itemStack) {
-        return CrustyChunksModItems.HUGE_BULLET.get().getDefaultInstance();
+        return CrustyChunksModItems.HUGE_PROJECTILE.get().getDefaultInstance();
     }
 
     @Override
