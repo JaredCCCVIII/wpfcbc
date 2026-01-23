d67f16fdb7fc56656c274df0720cfcc657a906a3
Jared308 <esqueleto300806@gmail.com>
2026-01-23 13:52:44 -0500

    Last update

--- src/main/java/net/ato/shupapium/blockentities/AbstractShupapiumBCProjectile.java
+++ src/main/java/net/ato/shupapium/blockentities/AbstractShupapiumBCProjectile.java
@@ -2,12 +2,16 @@ package net.ato.shupapium.blockentities;
 
 import com.tterrag.registrate.util.entry.BlockEntry;
 import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import rbasamoyai.createbigcannons.index.CBCMunitionPropertiesHandlers;
 import rbasamoyai.createbigcannons.munitions.big_cannon.FuzedBigCannonProjectile;
